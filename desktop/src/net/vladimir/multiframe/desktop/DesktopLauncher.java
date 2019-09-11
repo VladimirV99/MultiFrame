ea8b22588a528a589bfff99f0457e2c8fafd4fb5
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2019-09-11 14:34:14 +0200

    Decrease Window Size on Desktop

--- desktop/src/net/vladimir/multiframe/desktop/DesktopLauncher.java
+++ desktop/src/net/vladimir/multiframe/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import net.vladimir.multiframe.MultiFrame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
+		config.width = 960;
+		config.height = 540;
 		new LwjglApplication(new MultiFrame(), config);
 	}
 }
